9daa0748bd0d17b247941f209360aae4b4da33d2
betaqi <3188864257@qq.com>
2025-09-07 10:38:11 +0800

    feat: 首次提交

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+
+<template>
+  <div class="w-full flex flex-col gap-[10px] lg:gap-[30px]">
+    <img src="./assets/images/1.png" alt="">
+    <img src="./assets/images/2.png" alt="">
+    <img src="./assets/images/3.png" alt="">
+    <img src="./assets/images/4.png" alt="">
+    <img src="./assets/images/5.png" alt="">
+    <img src="./assets/images/6.png" alt="">
+    <img src="./assets/images/7.png" alt="">
+    <img src="./assets/images/8.png" alt="">
+    <img src="./assets/images/9.png" alt="">
+    <img src="./assets/images/10.png" alt="">
+    <img src="./assets/images/11.png" alt="">
+    <img src="./assets/images/12.png" alt="">
+    <img src="./assets/images/13.png" alt="">
+    <img src="./assets/images/14.png" alt="">
+    <img src="./assets/images/15.png" alt="">
+    <img src="./assets/images/16.png" alt="">
+    <img src="./assets/images/17.png" alt="">
+    <img src="./assets/images/bianjiqi.png" alt="">
+    <img src="./assets/images/20.png" alt="">
+    <img src="./assets/images/21.png" alt="">
+    <div class="relative">
+      <img src="./assets/images/22.png" alt="">
+      <img class="absolute w-[66.6%] top-[12.5%] left-[16.5%]" src="./assets/images/zuizhong.gif"
+           alt="">
+    </div>
+    <img src="./assets/images/23.png" alt="">
+    <img src="./assets/images/24.png" alt="">
+    <img src="./assets/images/25.png" alt="">
+    <img src="./assets/images/26.png" alt="">
+    <img src="./assets/images/27.png" alt="">
+    <img src="./assets/images/28.png" alt="">
+    <img src="./assets/images/29.png" alt="">
+    <img src="./assets/images/30.png" alt="">
+    <img src="./assets/images/31.png" alt="">
+    <img src="./assets/images/32.png" alt="">
+    <img src="./assets/images/33.png" alt="">
+    <img src="./assets/images/34.png" alt="">
+    <img src="./assets/images/35.png" alt="">
+    <img src="./assets/images/36.png" alt="">
+    <img src="./assets/images/37.png" alt="">
+  </div>
+
+</template>
+<script setup>
+</script>
+
+
+<style scoped></style>
